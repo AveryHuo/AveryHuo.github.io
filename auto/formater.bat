955f386f56a4df56103f02be8506b4f3e045d06d
huojiapeng <huojiapeng@corp.netease.com>
2022-05-20 17:13:31 +0800

    add bat files

---
+++ auto/formater.bat
@@ -0,0 +1,2 @@
+py readandupdatetime.py D:\Avery\Github\AveryHuo.github.io D:\Avery\Github\AveryHuo.github.io\source\_posts
+pause
---
+++ auto/formater.sh
@@ -0,0 +1,2 @@
+py readandupdatetime.py /data/blog/AveryHuo.github.io /data/blog/source/_posts
+pause
